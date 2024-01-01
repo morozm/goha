a2cead4c416284659088249b27230f77e41e5e3b
Milosz Moroz <milosz.moroz@pw.edu.pl>
2024-01-01 17:23:23 +0100

    3 new themes, spanish, french, german added

--- goha/constants.py
+++ goha/constants.py
@@ -141,6 +141,24 @@ T3_GREEN2 = (46, 238, 44)
 T3_GREEN3 = (123, 153, 114)
 T3_GREEN4 = (33, 53, 28)
 
+#theme 4
+T4_BLACK1 = (32, 42, 37)
+T4_BLUE1 = (95, 75, 182)
+T4_BLUE2 = (134, 165, 217)
+T4_BLUE3 = (38, 240, 241)
+
+#theme 5
+T5_PINK1 = (134, 22, 87)
+T5_PINK2 = (213, 106, 160)
+T5_GREEN1 = (187, 219, 180)
+T5_YELLOW1 = (252, 240, 204)
+
+#theme 6
+T6_BLACK1 = (26, 29, 26)
+T6_GREEN1 = (187, 219, 180)
+T6_BLUE1 = (123, 178, 217)
+T6_GREEN2 = (38, 65, 60)
+
 # stone colors
 STONECOLORS = {
     1: BLACKCOLOR,
@@ -151,17 +169,20 @@ STONECOLORS = {
 }
 
 THEMES_LIST = [
-    'Theme1', 'Theme2', 'Theme3'
+    'Theme1', 'Theme2', 'Theme3', 'Theme4', 'Theme5', 'Theme6'
 ]
 
 LANGUAGES_LIST = [
-    'English', 'Polski'
+    'English', 'Polski', 'Español', 'Français', 'Deutsch',
 ]
 
 THEMES = {
-    'Theme1': {'backgroundcolor': WHITECOLOR,       'maincolor1': BLACKCOLOR,       'maincolor2': LIGHTGREYCOLOR,   'cancelcolor': REDCOLOR2,   'iconbackgroundcolor': BROWNCOLOR},
-    'Theme2': {'backgroundcolor': T2_ORANGECOLOR2,  'maincolor1': T2_ORANGECOLOR1,  'maincolor2': T2_BROWNCOLOR3,   'cancelcolor': REDCOLOR2,   'iconbordercolor': BROWNCOLOR},
-    'Theme3': {'backgroundcolor': T3_GREEN1,        'maincolor1': T3_GREEN2,        'maincolor2': T3_GREEN3,        'cancelcolor': T3_GREEN4,   'iconbordercolor': BROWNCOLOR}
+    'Theme1': {'backgroundcolor': WHITECOLOR,       'maincolor1': BLACKCOLOR,       'maincolor2': LIGHTGREYCOLOR,   'cancelcolor': REDCOLOR2},
+    'Theme2': {'backgroundcolor': T2_ORANGECOLOR2,  'maincolor1': T2_ORANGECOLOR1,  'maincolor2': T2_BROWNCOLOR3,   'cancelcolor': REDCOLOR2},
+    'Theme3': {'backgroundcolor': T3_GREEN1,        'maincolor1': T3_GREEN2,        'maincolor2': T3_GREEN3,        'cancelcolor': T3_GREEN4},
+    'Theme4': {'backgroundcolor': T4_BLUE1,         'maincolor1': T4_BLACK1,        'maincolor2': T4_BLUE2,         'cancelcolor': T4_BLUE2},
+    'Theme5': {'backgroundcolor': T5_PINK2,         'maincolor1': T5_PINK1,         'maincolor2': T5_YELLOW1,       'cancelcolor': T5_GREEN1},
+    'Theme6': {'backgroundcolor': T6_GREEN1,        'maincolor1': T6_BLACK1,        'maincolor2': T6_BLUE1,         'cancelcolor': T6_GREEN2},
 }
 
 LANGUAGES = {
@@ -169,7 +190,7 @@ LANGUAGES = {
                  'Nick': 'Nick',     'Theme': 'Theme',   'Language': 'Language', 'Stone Centering': 'Stone Centering',       'Volume': 'Volume',
                  'Bot Difficulty': 'Bot Difficulty',    'Play As': 'Play As',   'Handicap': 'Handicap', 'Time': 'Time', 'Board Size': 'Board Size',
                  'Difficulties': ['Easy', 'Medium', 'Hard', 'Random', 'Play Solo'],    'PlayerColors': ['Black', 'White'], 'Handicaps': ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],   'Times': ['No', '10+0', '20+5'],    'BoardSizes': ['9x9', '13x13', '19x19'],
-                 'Themes': ['Theme 1', 'Theme 2', 'Theme 3'], 'Languages': ['English', 'Polish'],
+                 'Themes': ['Theme 1', 'Theme 2', 'Theme 3', 'Theme 4', 'Theme 5', 'Theme 6'], 'Languages': ['English', 'Polish', 'Spanish', 'French', 'German'],
                  'Info Text': '''Welcome to the game of Go! This ancient Chinese board game\\is a combination of science, art and sport. Try to cover as\\much territory as possible. Capture your opponent's stones,\\but don't let him capture yours!\\\\Hi, my name is Miłosz Moroz and I've created this program\\as a part of my Engineering Thesis. Try different opponents,\\different board sizes and adjust the app settings to your preferences.\\But above all, don't forget the most important thing - to have fun!''',
                  'Save': 'Save',    'Cancel': 'Cancel', 'OK': 'OK', 'Start': 'Start',
                  'Game ends.': 'Game ends.', 'Player resigned.': 'Player resigned.', 'Game ends. White won.': 'Game ends. White won.', '(Black ran out of time.)': '(Black ran out of time.)', 'Game ends. Black won.': 'Game ends. Black won.', '(White ran out of time.)': '(White ran out of time.)'},
@@ -177,8 +198,32 @@ LANGUAGES = {
                  'Nick': 'Ksywka',  'Theme': 'Motyw',   'Language': 'Język',    'Stone Centering': 'Środkowanie Kamieni',   'Volume': 'Głośność',
                  'Bot Difficulty': 'Trudność Bota',    'Play As': 'Graj Jako',   'Handicap': 'Handicap', 'Time': 'Czas', 'Board Size': 'Wielkość Planszy',
                  'Difficulties': ['Łatwy', 'Średni', 'Trudny', 'Losowy', 'Graj Sam'], 'PlayerColors': ['Czarny', 'Biały'], 'Handicaps': ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],  'Times': ['Nie', '10+0', '20+5'],   'BoardSizes': ['9x9', '13x13', '19x19'],
-                 'Themes': ['Motyw 1', 'Motyw 2', 'Motyw 3'], 'Languages': ['Angielski', 'Polski'],
+                 'Themes': ['Motyw 1', 'Motyw 2', 'Motyw 3', 'Motyw 4', 'Motyw 5', 'Motyw 6'], 'Languages': ['Angielski', 'Polski', 'Hiszpański', 'Francuski', 'Niemiecki'],
                  'Info Text': '''Witaj w grze w go! Ta starochińska gra planszowa to\\połączenie nauki, sztuki i sportu. Postaraj się otoczyć\\jak największe terytorium. Zbijaj kamienie przeciwnika,\\lecz nie pozwól, by on zbił twoje!\\\\Hej, jestem Miłosz Moroz i to właśnie ja stworzyłem ten\\program w ramach pracy inżynierskiej. Wypróbuj różnych\\przeciwników, różne rozmiary planszy i dostosuj ustawienia\\aplikacji według swoich preferencji. Lecz przede wszystkim\\nie zapomnij o najważniejszym - dobrze się bawić!''',
                  'Save': 'Zapisz',  'Cancel': 'Anuluj', 'OK': 'OK', 'Start': 'Start',
                  'Game ends.': 'Koniec gry.', 'Player resigned.': 'Gracz zrezygnował.', 'Game ends. White won.': 'Koniec gry. Białe wygrały.', '(Black ran out of time.)': '(Koniec czasu czarnych.)', 'Game ends. Black won.': 'Koniec gry. Czarne wygrały.', '(White ran out of time.)': '(Koniec czasu białych.)'},
+    'Español':  {'Continue': 'Continuar', 'New Game': 'Nueva Partida', 'Settings': 'Configuración', 'Info': 'Información', 'Exit': 'Salir',
+                 'Nick': 'Apodo', 'Theme': 'Tema', 'Language': 'Idioma', 'Stone Centering': 'Centrado de Piedras', 'Volume': 'Volumen',
+                 'Bot Difficulty': 'Dificultad del Bot', 'Play As': 'Jugar Como', 'Handicap': 'Desventaja', 'Time': 'Tiempo', 'Board Size': 'Tamaño del Tablero',
+                 'Difficulties': ['Fácil', 'Medio', 'Difícil', 'Aleatorio', 'Jugar Solo'], 'PlayerColors': ['Negro', 'Blanco'], 'Handicaps': ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], 'Times': ['No', '10+0', '20+5'], 'BoardSizes': ['9x9', '13x13', '19x19'],
+                 'Themes': ['Tema 1', 'Tema 2', 'Tema 3', 'Tema 4', 'Tema 5', 'Tema 6'], 'Languages': ['Inglés', 'Polaco', 'Español', 'Francés', 'Alemán'],
+                 'Info Text': '''¡Bienvenido al juego de Go! Este antiguo juego de\\mesa chino es una combinación de ciencia, arte y deporte.\\Intenta cubrir la mayor cantidad de territorio posible.\\Captura las piedras de tu oponente, pero no dejes que capture las tuyas.\\\\Hola, mi nombre es Miłosz Moroz y he creado este programa\\como parte de mi Tesis de Ingeniería.\\Prueba diferentes oponentes, tamaños de tablero diferentes\\y ajusta la configuración de la aplicación según tus preferencias.\\Pero, sobre todo, no olvides lo más importante: ¡divertirte!''',
+                 'Save': 'Guardar', 'Cancel': 'Cancelar', 'OK': 'Aceptar', 'Start': 'Comenzar',
+                 'Game ends.': 'Fin de la partida.', 'Player resigned.': 'Jugador resignado.', 'Game ends. White won.': 'Fin de la partida. Blancas ganaron.', '(Black ran out of time.)': '(Negras se quedaron sin tiempo.)', 'Game ends. Black won.': 'Fin de la partida. Negras ganaron.', '(White ran out of time.)': '(Blancas se quedaron sin tiempo.)'},
+    'Français': {'Continue': 'Continuer', 'New Game': 'Nouvelle Partie', 'Settings': 'Paramètres', 'Info': 'Info', 'Exit': 'Quitter',
+                 'Nick': 'Pseudo', 'Theme': 'Thème', 'Language': 'Langue', 'Stone Centering': 'Centrage des Pierres', 'Volume': 'Volume',
+                 'Bot Difficulty': 'Difficulté du Bot', 'Play As': 'Jouer En Tant Que', 'Handicap': 'Handicap', 'Time': 'Temps', 'Board Size': 'Taille du Plateau',
+                 'Difficulties': ['Facile', 'Moyen', 'Difficile', 'Aléatoire', 'Jouer Seul'], 'PlayerColors': ['Noir', 'Blanc'], 'Handicaps': ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], 'Times': ['Non', '10+0', '20+5'], 'BoardSizes': ['9x9', '13x13', '19x19'],
+                 'Themes': ['Thème 1', 'Thème 2', 'Thème 3', 'Thème 4', 'Thème 5', 'Thème 6'], 'Languages': ['Anglais', 'Polonais', 'Espagnol', 'Français', 'Allemande'],
+                 'Info Text': '''Bienvenue dans le jeu de Go ! Ce jeu de plateau\\chinois ancien est une combinaison de science, d'art et de sport.\\Essayez de couvrir autant de territoire que possible.\\Capturez les pierres de votre adversaire, mais ne laissez pas le vôtre être capturé !\\\\Salut, je m'appelle Miłosz Moroz et j'ai créé ce programme\\dans le cadre de ma thèse d'ingénierie. Essayez différents adversaires,\\différentes tailles de plateau et ajustez les paramètres de l'application selon vos préférences.\\Mais surtout, n'oubliez pas la chose la plus importante : vous amuser !''',
+                 'Save': 'Enregistrer', 'Cancel': 'Annuler', 'OK': 'OK', 'Start': 'Démarrer', 'Game ends.': 'Fin de la partie.', 'Player resigned.': 'Joueur démissionné.',
+                 'Game ends. White won.': 'Fin de la partie. Blanc a gagné.', '(Black ran out of time.)': '(Noir a manqué de temps.)', 'Game ends. Black won.': 'Fin de la partie. Noir a gagné.', '(White ran out of time.)': '(Blanc a manqué de temps.)'},
+    'Deutsch':  {'Continue': 'Weiter', 'New Game': 'Neues Spiel', 'Settings': 'Einstellungen','Info': 'Info', 'Exit': 'Beenden',
+                 'Nick': 'Spitzname', 'Theme': 'Thema', 'Language': 'Sprache', 'Stone Centering': 'Stein-Zentrierung', 'Volume': 'Lautstärke',
+                 'Bot Difficulty': 'Bot-Schwierigkeit', 'Play As': 'Spielen Als', 'Handicap': 'Handicap', 'Time': 'Zeit', 'Board Size': 'Brettgröße',
+                 'Difficulties': ['Leicht', 'Mittel', 'Schwer', 'Zufällig', 'Alleine Spielen'], 'PlayerColors': ['Schwarz', 'Weiß'], 'Handicaps': ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], 'Times': ['Nein', '10+0', '20+5'], 'BoardSizes': ['9x9', '13x13', '19x19'],
+                 'Themes': ['Thema 1', 'Thema 2', 'Thema 3', 'Thema 4', 'Thema 5', 'Thema 6'], 'Languages': ['Englisch', 'Polnisch', 'Spanisch', 'Französisch', 'Deutsch'],
+                 'Info Text': 'Willkommen beim Spiel Go! Dieses alte chinesische Brettspiel\\ist eine Kombination aus Wissenschaft, Kunst und Sport.\\Versuchen Sie, so viel Gebiet wie möglich zu bedecken.\\Erfassen Sie die Steine Ihres Gegners, lassen Sie jedoch nicht zu, dass er Ihre erfasst!\\\\Hallo, mein Name ist Miłosz Moroz und ich habe dieses Programm im Rahmen meiner\\Ingenieurarbeit erstellt. Probieren Sie verschiedene Gegner,\\verschiedene Brettgrößen aus und passen\\Sie die App-Einstellungen an Ihre Vorlieben an.\\Aber vor allem, vergessen Sie nicht das Wichtigste: Spaß zu haben!',
+                 'Save': 'Speichern', 'Cancel': 'Abbrechen', 'OK': 'OK', 'Start': 'Starten', 'Game ends.': 'Spielende.', 'Player resigned.': 'Spieler hat aufgegeben.',
+                 'Game ends. White won.': 'Spielende. Weiß hat gewonnen.', '(Black ran out of time.)': '(Schwarz ist die Zeit ausgegangen.)', 'Game ends. Black won.': 'Spielende. Schwarz hat gewonnen.', '(White ran out of time.)': '(Weiß ist die Zeit ausgegangen.)'},
 }
